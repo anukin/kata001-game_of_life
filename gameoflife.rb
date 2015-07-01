bf49a8e4f244a660c169ead725528e01fe098ae8
anukin <anukin2611@gmail.com>
2015-07-01 16:15:57 +0530

    Board configuration completed

--- gameoflife.rb
+++ gameoflife.rb
@@ -20,5 +20,16 @@ class Board
 		(1..n).each{temp << Cell.new}
 		(1..m).each{@board << temp}
 	end
+	def get_livecells
+		puts "Pls enter no of cells you wish to change \n"
+		n=gets.chomp.to_i
+		(1..n).each{self.change}
+	end
+	def change
+		puts "Enter the co-ordinates for live cells \n"
+		x=gets.chomp.to_i
+		y=gets.chomp.to_i
+		@board[x][y].state="live"
+	end
 
 end
